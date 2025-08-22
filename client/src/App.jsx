0c8e1e8f197a7f9e5336f1df97dbfa5da3d76fde
hasanalj0923 <hasanhnaljarrah@gmail.com>
2025-08-23 00:43:34 +0100

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Courses from "./components/Courses";
+import CourseDetail from "./components/CourseDetail";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from "./components/UpdateCourse";
+import UserSignIn from "./components/UserSignIn";
+import UserSignUp from "./components/UserSignUp";
+import UserSignOut from "./components/UserSignOut";
+import PrivateRoute from "./components/PrivateRoute";
+import NotFound from "./components/NotFound";
+import Forbidden from "./components/Forbidden";
+import UnhandledError from "./components/UnhandledError";
+
+const App = () => (
+  <>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<Courses />} />
+      <Route path="courses/:id" element={<CourseDetail />} />
+      <Route path="signin" element={<UserSignIn />} />
+      <Route path="signup" element={<UserSignUp />} />
+      <Route path="signout" element={<UserSignOut />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route path="courses/create" element={<CreateCourse />} />
+        <Route path="courses/:id/update" element={<UpdateCourse />} />
+      </Route>
+
+      <Route path="forbidden" element={<Forbidden />} />
+      <Route path="error" element={<UnhandledError />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </>
+);
+
+export default App;
