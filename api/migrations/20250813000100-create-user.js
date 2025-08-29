e3fafc1d8512babf644093c6ad69c5cfea82f873
hasanalj0923 <hasanhnaljarrah@gmail.com>
2025-08-29 20:16:27 +0100

    Add files via upload

--- api/migrations/20250813000100-create-user.js
+++ api/migrations/20250813000100-create-user.js
@@ -1,45 +1,44 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('Users', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            firstName: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            lastName: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            emailAddress: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: true
-            },
-            password: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.fn('NOW')
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.fn('NOW')
-            }
-        });
-    },
-    async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Users');
-    }
+  async up(queryInterface) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: 'INTEGER'
+      },
+      firstName: {
+        type: 'STRING',
+        allowNull: false
+      },
+      lastName: {
+        type: 'STRING',
+        allowNull: false
+      },
+      emailAddress: {
+        type: 'STRING',
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: 'STRING',
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: 'DATE',
+        defaultValue: queryInterface.sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: 'DATE',
+        defaultValue: queryInterface.sequelize.fn('NOW')
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
 };
-u 
--- api/migrations/20250813000200-create-course.js
+++ api/migrations/20250813000200-create-course.js
@@ -1,53 +1,53 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('Courses', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            title: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            description: {
-                type: Sequelize.TEXT,
-                allowNull: false
-            },
-            estimatedTime: {
-                type: Sequelize.STRING,
-                allowNull: true
-            },
-            materialsNeeded: {
-                type: Sequelize.STRING,
-                allowNull: true
-            },
-            userId: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: 'Users',
-                    key: 'id'
-                },
-                onUpdate: 'CASCADE',
-                onDelete: 'CASCADE'
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.fn('NOW')
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.fn('NOW')
-            }
-        });
-    },
-    async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable('Courses');
-    }
+  async up(queryInterface) {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: 'INTEGER'
+      },
+      title: {
+        type: 'STRING',
+        allowNull: false
+      },
+      description: {
+        type: 'TEXT',
+        allowNull: false
+      },
+      estimatedTime: {
+        type: 'STRING',
+        allowNull: true
+      },
+      materialsNeeded: {
+        type: 'STRING',
+        allowNull: true
+      },
+      userId: {
+        type: 'INTEGER',
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: 'DATE',
+        defaultValue: queryInterface.sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: 'DATE',
+        defaultValue: queryInterface.sequelize.fn('NOW')
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Courses');
+  }
 };
