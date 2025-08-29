0c0fc1f296d3d7fa233d05de3723697553d4d477
hasanalj0923 <hasanhnaljarrah@gmail.com>
2025-08-29 20:51:09 +0100

    Add files via upload

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,28 +1,48 @@
+// client/eslint.config.js
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import babelParser from '@babel/eslint-parser';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 
-export default defineConfig([
-  globalIgnores(['dist', 'node_modules']),
+export default [
+  { ignores: ['node_modules', 'dist'] },
+
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite
-    ],
     languageOptions: {
-      ecmaVersion: 'latest',
-      globals: globals.browser,
+      parser: babelParser,
       parserOptions: {
-        ecmaFeatures: { jsx: true },
+        requireConfigFile: false,
+        babelOptions: { presets: ['@babel/preset-react'] },
+        ecmaVersion: 2020,
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
       },
+      globals: globals.browser,
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // React 17+ doesn't require React in scope
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      // Allow unused vars for imports used only in JSX
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern:
+            'React|Routes|Route|Link|BrowserRouter|AuthProvider|Outlet|Navigate|Header|Courses|CourseDetail|CreateCourse|UpdateCourse|UserSignIn|UserSignUp|App',
+        },
+      ],
+    },
+    settings: {
+      react: { version: 'detect' },
     },
   },
-]);
+];
